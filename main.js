cb7c6ac02943eb0e09169a4bbef8b5aaffd66c7c
Olek <23708759+Roguezilla@users.noreply.github.com>
2019-05-09 10:08:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let walls = [];
+let p;
+
+function setup() {
+	createCanvas(600, 480);
+	for (let i = 0; i < 5; i++) {
+    	let x1 = random(width);
+    	let x2 = random(width);
+    	let y1 = random(height);
+    	let y2 = random(height);
+    	walls.push(new Boundary(x1, y1, x2, y2));
+  	}
+  	p = new Particle();
+}
+
+function draw() {
+	background(0);
+	for(let wall of walls) {
+		wall.show(255, 255, 255);
+	}
+	p.setPos(mouseX, mouseY);
+	p.show();
+	p.testHit(walls);
+}
---
+++ particle.js
@@ -0,0 +1,42 @@
+class Particle {
+	constructor() {
+        this.rays = [];
+        this.pos = createVector(0, 0);
+		for (let a = 0; a < 360; a += 10) {
+            this.rays.push(new Ray(this.pos, radians(a)));
+        }
+	}
+
+	setPos(x, y) {
+		this.pos.set(x, y);
+	}
+
+	testHit(walls) {
+        for (let ray of this.rays) {
+            let closest = null;
+            let max = Infinity;
+            for(let wall of walls) {
+                const pt = ray.cast(wall);
+                if (pt) {
+                    fill(255);
+                    let dist = p5.Vector.dist(this.pos, pt);
+                    if(dist < max) {
+                        closest = pt;
+                        max = dist;
+                    }
+                }
+            }
+            if(closest) {
+                line(this.pos.x, this.pos.y, closest.x, closest.y, 8, 8);
+            }
+        }
+    }
+
+    show() {
+        fill(255);
+        //ellipse(this.pos.x, this.pos.y, 4);
+        for(let ray of this.rays) {
+            ray.show();
+        }
+    }
+}
